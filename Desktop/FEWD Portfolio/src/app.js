bd5b958beac44491fcb217dd616fa9f2102c4f79
Alyshia Ho <alyshiahcr@gmail.com>
2022-12-15 22:44:14 +1100

    Final Changes

---
+++ Desktop/FEWD Portfolio/src/app.js
@@ -0,0 +1,9 @@
+const hambugerButton = document.getElementById('hamburger')
+const navList = document.getElementById
+
+function toggleButton() {
+    navList.classList.toggle('show')
+
+}
+
+hamburgerButton.addEventListener('clicks', toggleButton)
